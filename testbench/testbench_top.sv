2b2e7aeb54c33bce00da9812ff0903b1104f77d4
Gabriel Villanova Novaes Magalhães <gabriel.magalhaes@virtus.ufcg.edu.br>
2023-07-02 18:41:35 -0300

    chore: moves pkg to vscale_mul_div_unit_pkg.sv

--- testbench/testbench_top.sv
+++ testbench/testbench_top.sv
@@ -32,21 +32,6 @@
 `include "vscale_mul_div.v"
 
 // ------------------------------------------------------------------------------
-// Define UVM testbench package.
-
-package vscale_mul_div_unit;
-
-   // Import UVM packages.
-   import uvm_pkg::*;
-
-   // ----------------------------------------
-   // I don't need to extend the sequencer.  Typedef will do.
-
-   typedef uvm_sequencer #(input_transaction) input_sequencer;
-
-endpackage : vscale_mul_div_unit
-
-// ------------------------------------------------------------------------------
 // Top-level module for unit test environment.
 
 module top;
