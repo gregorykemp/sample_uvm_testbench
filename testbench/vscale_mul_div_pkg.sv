3ec3d226a893e3cd46ea1e47462f019b8e235963
Gabriel Villanova Novaes Magalhães <gabriel.magalhaes@virtus.ufcg.edu.br>
2023-07-02 19:32:15 -0300

    chore: renames and cleans the vscale_mul_div pkg

---
+++ testbench/vscale_mul_div_pkg.sv
@@ -0,0 +1,15 @@
+// ------------------------------------------------------------------------------
+// Define UVM testbench package.
+
+// Import UVM packages.
+import uvm_pkg::*;
+
+package vscale_mul_div_pkg;
+
+   // Include UVM macros 
+   `include "uvm_macros.svh"
+
+   // Include testbench components and objects
+   
+
+endpackage : vscale_mul_div_pkg
